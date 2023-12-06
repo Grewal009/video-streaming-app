e91c972b2347f3501c12dbd0eb6f8b2a7d2fabfb
Aneel Grewal <aneel.grewal@hotmail.com>
2023-12-06 14:31:44 +0100

    Header component created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,32 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { FaYoutube } from "react-icons/fa6";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="m-2 p-2 flex justify-between">
+      <div className="flex">
+        <div className="w-10 h-10 mx-2 bg-green-300 rounded-full hover:bg-slate-200 flex justify-center items-center">
+          <RxHamburgerMenu size={20} className="cursor-pointer" />
+        </div>
+        <div className="mx-2 flex items-center">
+          <FaYoutube color="red" size={35} />
+          <span className="px-1 font-bold ">YouTube</span>
+        </div>
+      </div>
+      <div className="mx-2 flex justify-center items-center md:w-3/5 ">
+        <input
+          type="text"
+          placeholder="Search"
+          className="outline-offset-1 py-1 px-3 bg-slate-100 w-3/5 rounded-l-full border-2 border-gray-500"
+        />
+        <button className=" py-1 px-3 bg-slate-100 hover:bg-slate-200 rounded-r-full border-t-2 border-b-2 border-r-2 border-gray-500">
+          Search
+        </button>
+      </div>
+      <div className="mx-2 flex items-center">
+        <span>User</span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
