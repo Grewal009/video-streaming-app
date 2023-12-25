de556b62e0f7ba0ec901be158d1ba0eaa8fa427b
Aneel Grewal <aneel.grewal@hotmail.com>
2023-12-25 08:48:52 +0100

    Search suggestions functionality created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaYoutube } from "react-icons/fa6";
 
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { Link } from "react-router-dom";
+import { YOUTUBE_SEARCH_SUGGESTIONS_API } from "../utils/constants";
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
   const dispatch = useDispatch();
   const toggleSlider = () => {
     dispatch(toggleMenu());
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 250);
+
+    //clean up component during rerender
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_SUGGESTIONS_API + searchQuery);
+    const json = await data.json();
+    console.log(json[1]);
+    setSearchSuggestions(json[1]);
+  };
+
   return (
     <div className="m-2 p-2 flex justify-between">
       <div className="flex">
@@ -26,15 +49,40 @@ const Header = () => {
           </div>{" "}
         </Link>
       </div>
-      <div className="mx-2 flex justify-center items-center md:w-3/5 ">
-        <input
-          type="text"
-          placeholder="Search"
-          className="outline-offset-1 py-1 px-3 bg-slate-100 w-3/5 rounded-l-full border-2 border-gray-500"
-        />
-        <button className=" py-1 px-3 bg-slate-100 hover:bg-slate-200 rounded-r-full border-t-2 border-b-2 border-r-2 border-gray-500">
-          Search
-        </button>
+      <div className="w-5/12 flex flex-col justify-center items-center">
+        <div className="mx-2 flex justify-center items-center z-10 w-11/12">
+          <input
+            type="text"
+            placeholder="Search"
+            className="outline-offset-0 py-1 px-3 bg-slate-100 w-3/5  rounded-l-full border-2 border-gray-500 z-50"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+
+          <ul className="top-[55px] absolute rounded-2xl border border-gray-200 w-auto z-0 bg-white">
+            {showSuggestions &&
+              searchSuggestions.map((s, index) => (
+                <li
+                  key={index}
+                  className="px-3 py-1 shadow-md overflow-hidden hover:bg-gray-100"
+                >
+                  🔍 {s}
+                </li>
+              ))}
+          </ul>
+
+          <button className="py-1 px-3 bg-slate-100 hover:bg-slate-200 rounded-r-full border-t-2 border-b-2 border-r-2 border-gray-500">
+            Search
+          </button>
+        </div>
+        {/* <div className="mt-[95px] -ml-[15%] absolute bg-gray-300 border border-gray-600 w-2/12  z-0 rounded-2xl">
+          <ul className="">
+            <li className="px-3 py-1 shadow-md">Hello</li>
+            <li className="px-3 py-1">Hello</li>
+          </ul>
+        </div> */}
       </div>
       <div className="mx-2 flex items-center">
         <span>User</span>
