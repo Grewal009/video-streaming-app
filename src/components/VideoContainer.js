0471c3f87bf97d9d5032d7afcff80836cb9eb4c3
Aneel Grewal <aneel.grewal@hotmail.com>
2023-12-29 01:05:23 +0100

    Optimized Infinite-Scroll functionality added to VideoContainer component

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -12,8 +12,34 @@ const VideoContainer = () => {
   const videos = useSelector((store) => store.videos.video);
   if (!videos) return null;
 
+  const getMoreVideos = () => {
+    getVideos();
+  };
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_API);
+    const json = await data.json();
+    //console.log(json);
+    dispatch(addVideos(json.items));
+  };
+
+  const moreVideos = (event) => {
+    // console.log("event.target.scrollTop", event.target.scrollTop);
+    // console.log("event.target.offsetHeight", event.target.offsetHeight);
+    // console.log("event.target.scrollHeight", event.target.scrollHeight);
+    if (
+      event.target.scrollTop + event.target.offsetHeight >=
+      event.target.scrollHeight - 10
+    ) {
+      getMoreVideos();
+      //console.log("scroll...");
+    }
+  };
+
   return (
-    <div className="m-2 pt-1 flex flex-wrap h-screen overflow-y-scroll">
+    <div
+      className="m-2 pt-1 flex flex-wrap h-screen overflow-y-scroll"
+      onScroll={moreVideos}
+    >
       {videos.map((vid, index) => (
         <Link to={"/watch?v=" + vid.id} key={index} className="mx-2 my-2">
           <VideoCard info={vid} />
